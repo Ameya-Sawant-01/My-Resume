4aafa0ea9090fa3828fd3dc4c3e52e4295ec3240
Ameya-Sawant-01 <105216368+Ameya-Sawant-01@users.noreply.github.com>
2023-03-17 02:24:33 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'education',
+    component: EducationComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+    PortfolioComponent,
+    ContactComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
